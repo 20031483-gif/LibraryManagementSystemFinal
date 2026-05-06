2ee48a1452c210cf8772d123d22328c668d187dc
20033535-hub <20033535@students.koi.edu.au>
2026-05-06 22:18:02 +1000

    Add project files.

---
+++ LibraryManagementSystem/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ LibraryManagementSystem/Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
