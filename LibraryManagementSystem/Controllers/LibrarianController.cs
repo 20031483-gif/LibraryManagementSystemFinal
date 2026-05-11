0e0be8c46846911ffbc8c66981e9d1d1c4b6e782
Sachin Gurung <20032376@students.koi.edu.au>
2026-05-12 04:23:00 +1000

    Added Librarian Admin Module

---
+++ LibraryManagementSystem/Controllers/LibrarianController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class LibrarianController : Controller
+    {
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult ManageLibraryInfo()
+        {
+            return View();
+        }
+
+        public ActionResult BorrowingRules()
+        {
+            return View();
+        }
+    }
+}
